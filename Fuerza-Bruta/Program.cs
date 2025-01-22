b151d4d27a509eb64c80198b535a046f5590b49a
Daniel Marin <damapa2003@gmail.com>
2025-01-22 15:59:55 +0100

    no funciona

---
+++ Fuerza-Bruta/Program.cs
@@ -0,0 +1,67 @@
+using System.Security.Cryptography;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        String contraseniaHashed = GenerarHashSHA256("!!!Daniel");
+            
+        string[] listaRecortada = new string[40];
+        try
+        {
+            string[] contrasenias = File.ReadAllLines("..\\..\\..\\Password\\2151220-passwords.txt");
+            
+            Array.Copy(contrasenias, listaRecortada, contrasenias.Length);
+            
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("The file could not be read:");
+            Console.WriteLine(e.Message);
+        }
+        
+        fuerzaBruta(contraseniaHashed, listaRecortada);
+        
+    }
+    
+    static string GenerarHashSHA256(string texto)
+    {
+        // Crear el objeto SHA256
+        using (SHA256 sha256 = SHA256.Create())
+        {
+            // Convertir el texto a bytes
+            byte[] bytesTexto = Encoding.UTF8.GetBytes(texto);
+
+            // Calcular el hash
+            byte[] hashBytes = sha256.ComputeHash(bytesTexto);
+
+            // Convertir los bytes del hash a una representación hexadecimal
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in hashBytes)
+            {
+                sb.Append(b.ToString("x2")); // Formato hexadecimal (dos dígitos)
+            }
+
+            return sb.ToString();
+        }
+    }
+
+    static void fuerzaBruta(String contraseniaHashed, string[] listaRecortada)
+    {
+        Boolean flag = false;
+        
+        foreach (var password in listaRecortada)
+        {
+            if (contraseniaHashed == GenerarHashSHA256(password))
+            {
+                flag = true;
+            }
+        }
+
+        if (flag)
+        {
+            Console.WriteLine("Fuerza bruta con exito");
+        }
+    }
+}
